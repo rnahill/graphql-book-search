b1e732b9b6c6fc63f45688281be536c8f1a85435
rnahill <rei333lly@gmail.com>
2024-02-29 16:38:24 -0500

    added more typedefs

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -36,6 +36,23 @@ const typeDefs = `
         ): User
       }
 
+      type User {
+        _id: ID!
+        username: String!
+        email: String!
+        bookCount: Int
+        savedBooks: [Book]
+      }
+    
+      type Book {
+        bookId: String!
+        authors:[String]
+        description: String!
+        title: String!
+        image: String
+        link: String
+      }
+
 `;
 
 module.exports = typeDefs;
