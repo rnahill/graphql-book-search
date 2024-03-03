bf0ddc2daab9aa4b40f8ec351aff2787a01d4dd5
rnahill <rei333lly@gmail.com>
2024-03-03 13:18:35 -0500

    edited signupform code

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { ADD_USER } from '../utils/queries';
+import { useMutation } from '@apollo/client';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
@@ -28,7 +30,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await useMutation(ADD_USER);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
