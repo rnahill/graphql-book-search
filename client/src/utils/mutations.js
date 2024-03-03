bb024d9267192281346151ceb94da2fadc83b8cc
rnahill <rei333lly@gmail.com>
2024-03-03 12:56:41 -0500

    added comments to mutations code

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 
-// Login mutation
+// Login
 export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
@@ -15,7 +15,7 @@ export const LOGIN_USER = gql`
         }
     `;
 
-    //add user mutation
+    // Add a user
 export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
@@ -30,7 +30,7 @@ export const ADD_USER = gql`
 `;
 
 
-    // save book mutation
+    // Update server and save a book
 export const SAVE_BOOK = gql`
     mutation saveBook($bookId: String!, $description: String!, $title: String! $authors: [String], $image: String, $link: String) {
         saveBook(bookId: $bookId, description: $description, title: $title authors: $authors, image: $image, link: $link) {
@@ -50,7 +50,7 @@ export const SAVE_BOOK = gql`
     }
 `;
 
-    // remove book mutation
+    // Update server and remove a book
 export const REMOVE_BOOK = gql`
     mutation removeBook($bookId: String!) {
         removeBook(bookId: $bookId) {
